f3b065137c7f358920d6c4adea78f72bd910b4c2
Caoimhinmg <kevin.gaastra@gmail.com>
2019-04-05 15:53:10 -0500

    added shell file for creating mag_synth script

---
+++ Synthetics/mag_synth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Script Doc String
+"""
+#import system libraries
+import os,sys
+#import processing libraries
+import numpy as np
+from numpy.fft import fft,ifft,fftfreq
+import pandas as pd
+#import visualization libraries
+import matplotlib.pyplot as plt
+
+def create_magsynth():
+    """
+    Function Doc String
+    """
+    pass #put outline of code here
+
+
+
+if __name__=="__main__":
+    pass #script stuff
+
